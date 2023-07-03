e8d88861390f356779a0ee6ff0192722a7fd6eb5
Elootam <toma.lion@hotmail.fr>
2023-07-03 16:23:13 +0200

    First attempt on main scene, start on implementing props and character + start setup lights

---
+++ Assets/0_Scripts/EventSystem/GameEventListener.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CustomGameEvent : UnityEvent<Component, object>{}
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent gameEvent;
+    public CustomGameEvent response;
+    private void OnEnable() => gameEvent.RegisterListener(this);
+    private void OnDisable() => gameEvent.UnregisterListener(this);
+
+    public void OnEventRaised(Component sender, object data) => response.Invoke(sender,data);
+}

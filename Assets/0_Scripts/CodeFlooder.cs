7baec91dbcbbca69224910bce4f5c4afbf857169
Elootam <toma.lion@hotmail.fr>
2023-07-24 11:57:01 +0200

    scenes added/changed

--- Assets/0_Scripts/CodeFlooder.cs
+++ Assets/0_Scripts/CodeFlooder.cs
@@ -1,14 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CodeFlooder : MonoBehaviour
 {
-    [SerializeField] private int numberOfCalls = 1000;
+    [SerializeField] private int numberOfObjectFound = 10000;
+    
     private void Update()
     {
-        for (int i = 0; i < numberOfCalls; i++)
+        for (int count = 0; count < numberOfObjectFound; count++)
         {
             var array = FindObjectsOfType(typeof(GameObject));
-            Debug.Log(array.Length + " object found !");
+            foreach (var element in array)
+            {
+                Transform to = element.GetComponent<Transform>();
+            }
+
+            count += array.Length;
+            Debug.Log(count + " object found !");
         }
     }
 }
